a5d76729c4d0a5751e42184661ef489bd42c276d
EMMALINE SHEAHAN <SHEAHANE@G.COFC.EDU>
2017-08-01 11:56:33 -0700

    Add script for rasterzing polygons

---
+++ scripts/rasterize_polygons.R
@@ -0,0 +1,32 @@
+library(rgdal)
+library(sp)
+library(maps)
+library(maptools)
+library(raster)
+
+# create a global raster layer
+world <- map(database = "world", fill =T)
+world <- map2SpatialPolygons(world, IDs = world$names, CRS("+proj=longlat +datum=WGS84"))
+world <- spTransform(world, CRS("+proj=cea +units=km"))
+plot(world)
+world_raster <- raster(world)
+res(world_raster) <- 110
+
+# saving the world raster grid
+save(world_raster, file = './data/raster/world_raster.Rdata')
+
+# raster species polygons
+sp_files = dir('./data/polygon')
+sp_raster <- vector("list", length = length(sp_files))
+for (i in seq_along(sp_files)) {
+    # read in 
+    temp_poly = readOGR(dsn = paste("./data/polygon/", sp_files[i], sep=''),
+                        layer = "OGRGeoJSON")
+    # convert projection to cea
+    temp_poly = spTransform(temp_poly, CRS("+proj=cea +units=km"))
+    # add field that will provide values when rasterized
+    temp_poly@data$occur = 1
+    # rasterize
+    sp_raster[[i]] = rasterize(temp_poly, world_raster, field = 'occur')
+}
+  
