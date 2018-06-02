76fd67e865ecb0d2d290d6199c6a39dcd549e693
EMMALINE SHEAHAN <SHEAHANE@G.COFC.EDU>
2018-06-02 14:53:30 -0700

    fixed the issue to get a list of names at each resolution of which species are in the region

--- scripts/ecoregions.R
+++ scripts/ecoregions.R
@@ -19,18 +19,51 @@ tropical_atlantic <- spTransform(tropical_atlantic, CRS("+proj=cea +units=km"))
 central_indopacific <- spTransform(central_indopacific, CRS("+proj=cea +units=km"))
 
 # cropping species richness based on realms
-trop_atlantic_list <- vector("list", length = 6)
-for (i in 1:6) {
-     ras <- intersect(species_richness[[i]], tropical_atlantic)
-     trop_atlantic_list[[i]] <- ras
+
+# realm_crop function
+# x = output list
+# y = raster list
+# z = realm spatial object
+realm_crop <- function(x, y) {
+  temporary <- vector("list", length = 6)
+  for (i in 1:6) {
+    ras <- intersect(x[[i]], y)
+    temporary[[i]] <- ras
+  }
+  return(temporary)
 }
+
+trop_atlantic_list <- realm_crop(species_richness, tropical_atlantic)
 save(trop_atlantic_list, file = './data/raster/trop_atlantic_list.Rdata')
 load('./data/raster/trop_atlantic_list.Rdata')
 
-indopacific_list <- vector("list", length = 6)
-for (i in 1:6) {
-  ras <- intersect(species_richness[[i]], central_indopacific)
-  indopacific_list[[i]] <- ras
-}
+trop_atlantic_stack <- realm_crop(sp_res_stack, tropical_atlantic)
+save(trop_atlantic_stack, file = './data/raster/trop_atlantic_stack.Rdata')
+load('./data/raster/trop_atlantic_stack.Rdata')
+
+indopacific_list <- realm_crop(species_richness, central_indopacific)
 save(indopacific_list, file = './data/raster/indopacific_list.Rdata')
 load('./data/raster/indopacific_list.Rdata')
+
+indopacific_stack <- realm_crop(sp_res_stack, central_indopacific)
+save(indopacific_stack, file = './data/raster/indopacific_stack.Rdata')
+load('./data/raster/indopacific_stack.Rdata')
+
+# creating dir
+trop_atlantic_names <- vector("list", length = 6)
+for (i in 1:6) {
+  for (j in 1:534) {
+    #print(any(trop_atlantic_stack[[i]][[j]]@data@values==1))
+    if (sum(trop_atlantic_stack[[i]][[j]]@data@values==1,na.rm=T)>0)
+      {
+      trop_atlantic_names[[i]][[j]] <- trop_atlantic_stack[[i]][[j]]@data@names
+      } else {trop_atlantic_names[[i]][[j]] <- NA}
+  }
+}
+
+
+
+# phylogenetic metrics for realms
+source('./scripts/Phylogenetic_metrics.R')
+
+
