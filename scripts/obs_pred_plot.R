32e3e84878cf3dd457ba438a20b932c12f1b3902
Dan McGlinn <danmcglinn@gmail.com>
2020-10-22 13:47:10 -0400

    Add new global analysis observed vs predicted plot where NCH and ELH are seperated out in different facets

--- scripts/obs_pred_plot.R
+++ scripts/obs_pred_plot.R
@@ -117,26 +117,49 @@ out_long2 %>%
 
 ggsave('./figures/pred_obs_metrics.png', width = 7 * 2, height = 7 * 2)
 
-# obs pred for global alone
-out_long2 %>%
+
+# jitter x-position
+tmp <- out_long2
+rand <- runif(nrow(tmp), min = -.05, max = 0.05)
+tmp$pred <- tmp$pred + rand
+# split the variable model up into hypo and orig
+tmp$hypo <- ifelse(grepl('ELH', tmp$model), 'ELH', 'NCH')    
+tmp$orig <- ifelse(grepl('temperate', tmp$model), 'temperate', 'tropical')
+# fix labels on the variable rel
+tmp$rel <- gsub('Energy Gradient vs MRD', 'temperature vs MRD', tmp$rel)
+tmp$rel <- gsub('Richness vs MRD', 'richness vs MRD', tmp$rel)
+tmp$rel <- gsub('Richness vs Temperature', 'temperature vs richness', tmp$rel)
+# fix labels on the hypo
+hypo_labs <- c("Energy Limits Hypothesis", "Niche Conservatism Hypothesis") 
+names(hypo_labs) <- c("ELH", "NCH")
+
+tmp %>%
   subset(subset = scale == 4) %>%
   filter(analysis == "Global Analysis") %>%
   ggplot(aes(pred, obs)) +
-  geom_point(aes(col = model, pch = rel, cex = 5), alpha = 0.9) + 
   geom_abline(intercept = 0, slope = 1) +
+  geom_hline(yintercept = 0, lty = 2, col = 'grey') + 
+  geom_vline(xintercept = 0, lty = 2, col = 'grey') +     
   geom_errorbar(aes(ymin=lower_error, ymax=upper_error), width=.05, alpha = 0.5) +
-  labs(col = "Model", pch = "Metric") +
-  scale_color_manual(values = rev(wes_palette("GrandBudapest2"))) +
+  geom_point(aes(col = orig, pch = rel), cex = 2, stroke = 2) + 
+  scale_shape_manual(values = c(15, 16, 17, 18)) +
+  labs(col = "Center of Origin", pch = "Metric") +
+  scale_color_manual(values = c('blue', 'red')) +
   ylim(-1.5, 1.5) + 
   xlim(-1.5, 1.5) + 
   xlab("Predicted Values") +
   ylab("Observed Values") +
-  theme(strip.text.x = element_text(size = 15), axis.title.x = element_text(size = 14),
+  theme_classic() + 
+  theme(strip.background = element_blank(), strip.text = element_text(size = 10),
+        axis.title.x = element_text(size = 14),
         axis.title.y = element_text(size = 14), legend.text = element_text(size = 12),
-        legend.title = element_text(size = 14), aspect.ratio = 1.1) +
-  facet_wrap(. ~ analysis, scales = "free", nrow = 1)
+        legend.title = element_text(size = 14), aspect.ratio = 1.1, 
+        panel.spacing = unit(1, "lines")) +
+  facet_wrap(. ~ hypo, labeller = labeller(hypo = hypo_labs),
+             scales = "fixed", nrow = 1)
+ggsave('./figures/pred_obs_metrics_global_sepearted.png')
 
-ggsave('./figures/pred_obs_metrics_global.png')
+
 
 # obs pred for remainder without global
 out_long2 %>%
