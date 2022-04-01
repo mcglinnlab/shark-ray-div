abf70cdcae40eec6fe279b4f6ef9fdab9b9c3662
McGlinn <mcglinndj@cofc.edu>
2022-04-01 10:11:40 -0400

    Improve the predicted vs observed plots (fig 3). Code changes by E.Sheahan.

--- scripts/Final_graphic.R
+++ scripts/Final_graphic.R
@@ -20,11 +20,15 @@ load('./data/stats/trop_atlantic_richnessVtemp.Rdata')
 load('./data/stats/indopacific_richnessVmrd.Rdata')
 load('./data/stats/indopacific_tempVmrd.Rdata')
 load('./data/stats/indopacific_richnessVtemp.Rdata')
-load('./data/stats/sims.Rdata')
+load('./data/stats/cor_df_sims.Rdata')
+load('./data/stats/p_df_sims.Rdata')
+load('./data/stats/sharkb.Rdata')
+load('./data/stats/car_b.Rdata')
+load('./data/stats/lam_b.Rdata')
+load('./data/stats/trop_atalantic_b.Rdata')
+load('./data/stats/indo_pacific_b.Rdata')
 
-sims$bet[1] <- 0.4
-sims$bet[2] <- 0.4
-
+cor_df_sims <- data.frame(cor_df_sims)
 
 # for correlation coefficient
 indopacific_richnessVmrd[[1]][[4]]
@@ -32,33 +36,33 @@ indopacific_richnessVmrd[[1]][[4]]
 # create a data table where the columns are the analyses
 table_list <- vector("list", length = 6)
 for (i in 1:6) {
-  cn <- c("Metric", "Niche_Conservatism_(Temperate)", "Niche_Conservatism_(Tropical)", 
-          "Ecological_Limits_(Temperate)", "Ecological_Limits_(Tropical)", 
+  cn <- c("Metric", "Niche_Conservatism_(Tropical)", "Niche_Conservatism_(Temperate)", 
+          "Ecological_Limits_(Tropical)", "Ecological_Limits_(Temperate)", 
           "Global_Analysis", "Carcharhiniformes", "Lamniformes", 
           "Tropical_Atlantic", "Central_Indo-Pacific")
   rn <- c("Richness vs Temperature", "Richness vs MRD", 
           "Energy Gradient vs MRD", "Beta")
   temp_table <- matrix(ncol = 9, nrow = 4)
-  temp_table[1, 1:4] <- sims$richvtemp
-  temp_table[2, 1:4] <- sims$richvmrd
-  temp_table[3, 1:4] <- sims$energyvmrd
-  temp_table[4, 1:4] <- sims$bet
+  temp_table[1, 1:4] <- cor_df_sims$temp_v_rich
+  temp_table[2, 1:4] <- cor_df_sims$rich_v_mrd
+  temp_table[3, 1:4] <- cor_df_sims$mrd_v_temp
+  temp_table[4, 1:4] <- cor_df_sims$beta_stat
   temp_table[,5] <- c(tempVrichness_stats[[i]][[4]], 
                       mrdVrichness_stats[[i]][[4]], 
-                      tempVmrd_stats[[i]][[4]], -0.8868239)
+                      tempVmrd_stats[[i]][[4]], sharkb$max_lik)
   temp_table[,6] <- c(tempVcarrichness_stats[[i]][[4]], 
                       carmrdVcarrichness_stats[[i]][[4]], 
-                      tempVcarmrd_stats[[i]][[4]], -0.9441983)
+                      tempVcarmrd_stats[[i]][[4]], car_b$max_lik)
   temp_table[,7] <- c(tempVlamrichness_stats[[i]][[4]], 
                       lammrdVlamrichness_stats[[i]][[4]], 
-                      tempVlammrd_stats[[i]][[4]], -0.6364018)
+                      tempVlammrd_stats[[i]][[4]], lam_b$max_lik)
   temp_table[,8] <- c(trop_atlantic_richnessVtemp[[i]][[4]], 
                       trop_atlantic_richnessVmrd[[i]][[4]], 
-                      trop_atlantic_tempVmrd[[i]][[4]], -0.9621481)
+                      trop_atlantic_tempVmrd[[i]][[4]], trop_atlantic_b$max_lik)
   temp_table[,9] <- c(indopacific_richnessVtemp[[i]][[4]], 
                       indopacific_richnessVmrd[[i]][[4]], 
                       indopacific_tempVmrd[[i]][[4]], 
-                      -0.8929302)
+                      indo_pacific_b$max_lik)
   temp_table <- data.frame(temp_table)
   temp_table <- cbind(rn, temp_table)
   colnames(temp_table) <- cn
@@ -69,10 +73,12 @@ save(table_list, file = './data/stats/table_list.Rdata')
 
 # for error bars
 # [3] for correlation coefficient: upper
-confint(mod, "x", level=0.95)[2]
+# confint(mod, "x", level=0.95)[2]
+# sharkb$conf_interval[[2]]
 
 # lower
-confint(mod, "x", level=0.95)[1]
+# confint(mod, "x", level=0.95)[1]
+# sharkb$conf_interval[[1]]
 
 error_list <- vector("list", length = 6)
 for (i in 1:6) {
@@ -81,7 +87,9 @@ for (i in 1:6) {
   error_df[,1] <- c(rep("Beta", each = 5), rep("Energy Gradient vs MRD", each = 5), 
                     rep("Richness vs MRD", each = 5), rep("Richness vs Temperature", each = 5))
   error_df[,2] <- rep(c("GA", "Car", "Lam", "Trop_Atl", "Indo"), times = 4)
-  error_df[,3] <- c(-0.621985, -0.5738432, 6.558514, -0.5981778, -0.5615923, 
+  error_df[,3] <- c(sharkb$conf_interval[[2]], car_b$conf_interval[[2]], 
+                    lam_b$conf_interval[[2]], trop_atlantic_b$conf_interval[[2]], 
+                    indo_pacific_b$conf_interval[[2]], 
                       confint(tempVmrd_stats[[i]][[3]], "x_std", level = 0.95)[2], 
                       confint(tempVcarmrd_stats[[i]][[3]], "x_std", level = 0.95)[2], 
                       confint(tempVlammrd_stats[[i]][[3]], "x_std", level = 0.95)[2], 
@@ -98,7 +106,9 @@ for (i in 1:6) {
                       confint(trop_atlantic_richnessVtemp[[i]][[3]], "x_std", level = 0.95)[2],
                       confint(indopacific_richnessVtemp[[i]][[3]], "x_std", level = 0.95)[2])
   
-  error_df[,4] <- c(-1.1103004, -1.2345835, -2, -1.2485749, -1.1603735, 
+  error_df[,4] <- c(sharkb$conf_interval[[1]], car_b$conf_interval[[1]], 
+                    lam_b$conf_interval[[1]], trop_atlantic_b$conf_interval[[1]], 
+                    indo_pacific_b$conf_interval[[1]], 
                       confint(tempVmrd_stats[[i]][[3]], "x_std", level = 0.95)[1], 
                       confint(tempVcarmrd_stats[[i]][[3]], "x_std", level = 0.95)[1], 
                       confint(tempVlammrd_stats[[i]][[3]], "x_std", level = 0.95)[1], 
